543066c13824af9c2e0f80b13b7deecaa4d40a71
Sam Kirsch <samk@provokesolutions.com>
2018-07-27 16:26:06 -0700

    minor changes to spell service for users

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from '../../../node_modules/rxjs/BehaviorSubject';
 
 @Injectable()
 export class UserService {
 
   constructor() { }
+
+  private isSignedIn: BehaviorSubject<boolean> = new BehaviorSubject(false);
+  
 
 }
